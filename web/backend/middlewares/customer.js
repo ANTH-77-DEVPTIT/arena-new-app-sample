da51268eea1539e92eb5d1efc2d021814ad7ea81
Trương Hùng An <truonghungan2000@gmail.com>
2022-08-29 22:54:10 +0700

    coding customers, search

--- web/backend/middlewares/customer.js
+++ web/backend/middlewares/customer.js
@@ -13,16 +13,29 @@ const count = async ({ shop, accessToken }) => {
   }
 }
 
-const find = async ({ shop, accessToken }) => {
+const find = async ({ shop, accessToken, limit, pageInfo, order }) => {
   try {
     validateParams({ shop, accessToken })
 
     //handle pagination page and limit
+    let _limit = limit ? (parseInt(limit) >= 0 ? parseInt(limit) : 20) : 20
+
+    let endpoint = `products.json?limit=${_limit}`
+    if (pageInfo) {
+      endpoint += `&page_info=${pageInfo}`
+    } else {
+      if (order) {
+        endpoint += `&order=${order}`
+      } else {
+        endpoint += `&order=updated_at+desc`
+      }
+    }
 
     return await apiCaller({
       shop,
       accessToken,
       endpoint: `customers.json`,
+      pageInfo: true,
     })
   } catch (error) {
     throw error
